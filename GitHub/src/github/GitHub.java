619c4989fa53f3b67f9dd791b9b1aedeacfef421
Piero <Piero@DESKTOP-CI4PB7T>
2026-06-23 20:04:53 -0500

    prueba2

--- GitHub/src/github/GitHub.java
+++ GitHub/src/github/GitHub.java
@@ -14,7 +14,7 @@ public class GitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String x = "AAAA";
     }
     
 }
